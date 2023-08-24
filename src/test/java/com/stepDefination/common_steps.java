5a4a7e59c363c740359043ce73db3a083a85e29a
VeeraVk13 <104502747+VeeraVk13@users.noreply.github.com>
2023-08-24 17:49:27 +0530

    update 001

--- src/test/java/com/stepDefination/common_steps.java
+++ src/test/java/com/stepDefination/common_steps.java
@@ -1,5 +1,6 @@
 package com.stepDefination;
 
+import com.Utitles.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.xmlbeans.impl.xb.xsdschema.Public;
@@ -8,10 +9,6 @@ import org.openqa.selenium.TakesScreenshot;
 
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import com.Utitles.CommonUtils;
-import com.Utitles.Drivermanger;
-import com.Utitles.Globalvariables;
-
 import io.cucumber.java.After;
 import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
@@ -19,36 +16,39 @@ import io.cucumber.java.BeforeAll;
 import io.cucumber.java.Scenario;
 
 public class common_steps implements Globalvariables {
-	private static final Logger LOG = LogManager.getLogger(common_steps.class);
-	protected WebDriverWait wait;
+    private static final Logger LOG = LogManager.getLogger(common_steps.class);
 
-	@Before
-	public void beforeScenario() {
+    @Before
+    public void beforeScenario() {
 
-		try {
-			if (Drivermanger.getDriver() == null) {
-				Drivermanger.launchBrowser(Browser);
-			
+        try {
+            if (Drivermanger.getDriver() == null) {
 
-			}
-		} catch (Exception e) {
-			
-		}
+                Drivermanger.launchBrowser(Browser);
+                Screenrecoder.startScreenRecording(Drivermanger.getDriver());
+            }
+        } catch (Exception e) {
 
-	}
+        }
 
-	@After
-	public void afterScenario() {
-		Drivermanger.getDriver().close();
-	}
+    }
 
-	@AfterStep
-	public void attachScreenshot(Scenario scenario) {
-		if (scenario.isFailed()) {
-			final byte[] screenshot = ((TakesScreenshot) Drivermanger.getDriver()).getScreenshotAs(OutputType.BYTES);
-			scenario.attach(screenshot, "image/png", "errorscreen");
-		}
+    @After
+    public void afterScenario() {
+        Drivermanger.getDriver().close();
+        Screenrecoder.stopScreenRecording();
+        Drivermanger.getDriver().quit();
+    }
 
-	}
+    @AfterStep
+    public void attachScreenshot(Scenario scenario) {
+        if (scenario.isFailed()) {
+            final byte[] screenshot = ((TakesScreenshot) Drivermanger.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png", "errorscreen");
+        }
+
+    }
+
+
 
 }
